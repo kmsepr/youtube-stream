5138208e1ec0abea71b0ecd45491536b11fe74bf
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-07 19:24:03 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -79,7 +79,7 @@ def generate_stream(youtube_url):
 
         process = subprocess.Popen(
     ["ffmpeg", "-re", "-i", stream_url,
-     "-vn", "-acodec", "libmp3lame", "-b:a", "40k",
+     "-vn", "-acodec", "libmp3lame", "-b:a", "32k",
      "-buffer_size", "256k", "-f", "mp3", "-"],
     stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=8192
 )
